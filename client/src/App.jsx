70d4595fbbeb35c9f474dbe1040922952aee97df
LepharamRamchiary <lepharamchiary@gmail.com>
2025-10-14 11:22:55 +0530

    added simple routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
-    <div className="text-red-500">App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/view/:resumeId" element={<Preview />} />
 
-export default App
+        <Route path="/dashboard" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
