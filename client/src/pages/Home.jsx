5185d072a52bf61515658949f155512db3eea9b1
LepharamRamchiary <lepharamchiary@gmail.com>
2025-10-14 13:36:42 +0530

    added feature ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Banner from '../components/home/Banner'
-import Hero from '../components/home/Hero'
+import React from "react";
+import Banner from "../components/home/Banner";
+import Hero from "../components/home/Hero";
+import Features from "../components/home/Features";
+import Testimonials from "../components/home/Testimonials";
 
 const Home = () => {
   return (
     <div>
-      <Banner/>
-      <Hero/>
+      <Banner />
+      <Hero />
+      <Features />
+      <Testimonials />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
